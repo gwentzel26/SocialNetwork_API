b8acacd29ac5cd2a9ec66dd3572a5d2082df8653
Gavin Wentzel <gavinwentzel@gmail.com>
2023-03-20 17:09:39 -0400

    Updated changes

--- Develop/routes/api/userRoutes.js
+++ Develop/routes/api/userRoutes.js
@@ -13,7 +13,7 @@ const {
 router.route('/').get(getUser).post(createUser);
 
 // /api/users/:userId
-router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+router.route('/userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
 // /api/users/:userId/friends
 // router.route('/:userId/friends').post(addFriend);
